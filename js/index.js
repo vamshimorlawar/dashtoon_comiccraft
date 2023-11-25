561cb4c426a6fbcc7638b20e0ce32339638bd069
Vamshikiran Morlawar <vamshikiranmorlawar@Vamshikirans-MacBook-Air.local>
2023-11-25 22:12:46 +0530

    functions to open and close modal

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function openModal() {
+    // Display the overlay and modal
+    document.getElementById('overlay').style.display = 'flex';
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    // Hide the overlay and modal
+    document.getElementById('overlay').style.display = 'none';
+  }
